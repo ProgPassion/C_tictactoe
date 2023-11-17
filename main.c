b90c0599b52e050cfcb4d0475bdbca6565166a80
imran <31067252+ProgPassion@users.noreply.github.com>
2023-11-17 17:28:37 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+        printf("    1   2   3\n");
+        printf("   --- --- ---\n");
+        printf("1 |   |   |   |\n");
+        printf("  |   |   |   |\n");
+        printf("  |   |   |   |\n");
+        printf("   --- --- --- \n");
+        printf("2 |   |   |   |\n");
+        printf("  |   |   |   |\n");
+        printf("   --- --- --- \n");
+        printf("3 |   |   |   |\n");
+        printf("  |   |   |   |\n");
+        printf("   --- --- ---\n");
+        printf("\n\n");
+        printf("");
+}
+
