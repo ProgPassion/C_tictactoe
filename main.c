94a0a6c61751fc14ba18178e6455c253822678a5
gms.cchelpdesk <gms.cchelpdesk@gmail.com>
2023-11-24 18:52:39 +0100

    Sample code in C for a tic tac toe game

--- main.c
+++ main.c
@@ -1,19 +1,119 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
+void printField(char field[3][3]) {
         printf("    1   2   3\n");
         printf("   --- --- ---\n");
-        printf("1 |   |   |   |\n");
         printf("  |   |   |   |\n");
-        printf("  |   |   |   |\n");
-        printf("   --- --- --- \n");
-        printf("2 |   |   |   |\n");
+        printf("1 | %c | %c | %c |\n", field[0][0], field[0][1], field[0][2]);
         printf("  |   |   |   |\n");
         printf("   --- --- --- \n");
-        printf("3 |   |   |   |\n");
+        printf("  |   |   |   |\n");
+        printf("2 | %c | %c | %c |\n", field[1][0], field[1][1], field[1][2]);
+        printf("  |   |   |   |\n");
+        printf("   --- --- --- \n");
+        printf("  |   |   |   |\n");
+        printf("3 | %c | %c | %c |\n", field[2][0], field[2][1], field[2][2]);
         printf("  |   |   |   |\n");
         printf("   --- --- ---\n");
         printf("\n\n");
-        printf("");
 }
+
+void printPlayerMsg(int *x, int *y, char player) {
+        printf("Lojtari %c, vendosni numrin e koordinates x te kuadratit: ", player);
+        scanf("%d", x);
+        printf("Lojtari %c, vendosni numrin e koordinates y te kuadratit: ", player);
+        scanf("%d", y);
+}
+
+int writeField(int x, int y, char player, char field[3][3]) {
+        if(field[x][y] == ' ') {
+                field[x][y] = player;
+                return 1;
+        }
+        return 0;
+}
+
+void printWinner(char winner) {
+        if(winner == ' ') {
+                printf("There is no winner, its a draw!\n");
+        }
+        else {
+                printf("Player %c is the winner.\n", winner);
+        }
+}
+
+int checkGameOver(char field[3][3]) {
+        for(int i = 0; i < 3; i++) {
+                if(field[i][0] == field[i][1] && field[i][1] == field[i][2] && field[i][0] != ' ') {
+                        return 1;
+                }
+                
+        }
+
+        for(int i = 0; i < 3; i++) {
+                if(field[0][i] == field[1][i] && field[1][i] == field[2][i] && field[0][i] != ' ') {
+                        return 1;
+                }
+                
+        }
+
+        if ((field[0][0] == field[1][1] && field[1][1] == field[2][2] && field[0][0] != ' ') ||
+            (field[0][2] == field[1][1] && field[1][1] == field[2][0] && field[0][2] != ' ')) {
+                return 1;  
+        }
+
+        return 0;
+}
+
+int checkIfNoEmptyBoxRemained(char field[3][3]) {
+        for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    if(field[i][j] == ' ') {
+                        return 0;
+                    }    
+                }
+        }
+
+        return 1;
+}
+
+int main() {
+
+        int xCoor, yCoor;
+        int turn = 1;
+        char player = 'x';
+        char winner = ' ';
+        char field[3][3] = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+
+        while(1) {
+                system("clear");
+                if(checkIfNoEmptyBoxRemained(field)) {
+                        break;
+                }
+                if(turn % 2 == 1) {
+                        player = 'x';
+                }
+                else {
+                        player = 'o';
+                }
+                printField(field);
+                printPlayerMsg(&xCoor, &yCoor, player);
+                if(writeField(xCoor - 1, yCoor - 1, player, field)) {
+                        turn++;
+                }
+                if(checkGameOver(field)) {
+                        winner = player;
+                        break;
+                }
+        }
+        system("clear");
+        printField(field);
+        printWinner(winner);
+}
+
 
